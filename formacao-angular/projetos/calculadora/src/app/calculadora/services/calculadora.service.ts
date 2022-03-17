7be601d929f35fa83ca1509bb18943ad8fe4fb3b
prisciladuarte <duartecostap@gmail.com>
2022-03-17 16:57:25 -0300

    Implementa função

--- formacao-angular/projetos/calculadora/src/app/calculadora/services/calculadora.service.ts
+++ formacao-angular/projetos/calculadora/src/app/calculadora/services/calculadora.service.ts
@@ -1,3 +1,8 @@
+/**
+ * @since 2022
+ * @author Priscila Duarte - Mabiá
+ */
+
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +10,57 @@ import { Injectable } from '@angular/core';
 })
 export class CalculadoraService {
 
+  /* 
+    Define constantes utilizadas no projeto 
+    Para identificar as operações de cálculo
+  */
+  static readonly SOMA: string = '+';
+  static readonly SUBTRACAO: string = '-';
+  static readonly DIVISAO: string = '/';
+  static readonly MULTIPLICACAO: string = '*';
+
   constructor() { }
+
+  /**
+   * @param num1 number
+   * @param num2 number
+   * @param operacao string Operação a ser executada
+   * @return number Resultado da operação
+   */
+
+  calcular
+  (
+    num1: number,
+    num2: number,
+    operacao: string
+  ): number
+  {
+    let resultado: number; // armazena o resultado da operação
+  
+  switch (operacao) {
+    case CalculadoraService.SOMA:
+      resultado = num1 + num2;
+    break;
+    case CalculadoraService.SUBTRACAO:
+      resultado = num1 - num2;
+    break;
+    case CalculadoraService.DIVISAO:
+      resultado = num1 / num2;
+    break;
+    case CalculadoraService.MULTIPLICACAO:
+      resultado = num1 * num2;
+    break;
+    default:
+      resultado = 0;
+      break;
+  }
+  return resultado;
 }
+
+
+
+}
+
+
+
+
